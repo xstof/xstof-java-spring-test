93c8e9e73a0882fb6940c127e7d27a9d29bad13b
Christof <xstofgit@outlook.com>
2020-10-09 18:55:17 +0200

    add more tests

--- demo/src/main/java/com/example/demo/JWTSecurityConfig.java
+++ demo/src/main/java/com/example/demo/JWTSecurityConfig.java
@@ -12,11 +12,18 @@ public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+          //.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          //.and()
           .authorizeRequests(authz -> authz
-            .antMatchers(HttpMethod.GET, "/foo").authenticated()
+            .antMatchers(HttpMethod.GET, "/greeting").permitAll()
+            .antMatchers(HttpMethod.GET, "/foo").hasAnyAuthority("SCOPE_write")
+            .antMatchers(HttpMethod.POST, "/foo").hasRole("staff")
             //.antMatchers(HttpMethod.GET, "/foo/**").hasAuthority("SCOPE_read")
             //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("SCOPE_write")
             .anyRequest().anonymous())
           .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 	}
 }
+
+// TODO: a session cookie is still being set when accessing /foo - why ?
+// TODO: test by setting a jwt in the header
