579a76a2054e9433f7f59b3f2d7330b35e2713b4
Christof <xstofgit@outlook.com>
2020-10-11 18:37:25 +0200

    add endpoint for returning scopes

--- demo/src/main/java/com/example/demo/JWTSecurityConfig.java
+++ demo/src/main/java/com/example/demo/JWTSecurityConfig.java
@@ -4,26 +4,39 @@ import org.springframework.context.annotation.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.oauth2.server.resource.authentication.*;
 
-//@Configuration
+@Configuration
 @EnableWebSecurity
 public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
  
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-          //.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-          //.and()
-          .authorizeRequests(authz -> authz
-            .antMatchers(HttpMethod.GET, "/greeting").permitAll()
-            .antMatchers(HttpMethod.GET, "/foo").hasAnyAuthority("SCOPE_write")
-            .antMatchers(HttpMethod.POST, "/foo").hasRole("staff")
-            //.antMatchers(HttpMethod.GET, "/foo/**").hasAuthority("SCOPE_read")
-            //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("SCOPE_write")
-            .anyRequest().anonymous())
-          .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-	}
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      http
+        //.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        //.and()
+        .authorizeRequests(authz -> authz
+          .antMatchers(HttpMethod.GET, "/greeting").permitAll()
+          .antMatchers(HttpMethod.GET, "/me").permitAll()
+          .antMatchers(HttpMethod.GET, "/foo").hasAnyAuthority("SCOPE_write")
+          .antMatchers(HttpMethod.POST, "/foo").hasRole("staff")
+          //.antMatchers(HttpMethod.GET, "/foo/**").hasAuthority("SCOPE_read")
+          //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("SCOPE_write")
+          .anyRequest().anonymous())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+  }
+
+  @Bean
+  public JwtAuthenticationConverter AADJwtAuthenticationConverter() {
+    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+    return jwtAuthenticationConverter;
+  }
 }
+
 
 // TODO: a session cookie is still being set when accessing /foo - why ?
 // TODO: test by setting a jwt in the header
---
+++ demo/src/main/java/com/example/demo/MeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.*;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/me")
+public class MeController {
+
+    @GetMapping()
+    public Object getInfoAboutMe(@CurrentSecurityContext(expression = "authentication") Authentication authentication){
+        return authentication.getAuthorities().toArray();
+    }
+}
