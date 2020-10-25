621c8bc6363d942a2c71ad2a62c2d567c0fc857b
Christof <xstofgit@outlook.com>
2020-10-25 14:41:12 +0100

    set profile in launch.json instead

--- demo-spring-mvc/src/main/java/com/example/demospringmvc/OAuth2LoginSecurityConfig.java
+++ demo-spring-mvc/src/main/java/com/example/demospringmvc/OAuth2LoginSecurityConfig.java
@@ -1,8 +1,17 @@
 package com.example.demospringmvc;
 
-import org.springframework.context.annotation.Configuration;
+import java.util.*;
+
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 @Configuration
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +23,34 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
             )
             .oauth2Login();
+    }
+
+    // work with AAD application roles:
+    // see: https://docs.spring.io/spring-security/site/docs/5.4.1/reference/html5/#oauth2login-advanced
+    @Bean
+    GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (OidcUserAuthority.class.isInstance(authority)) {
+                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority)authority;
+
+                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
+                    // OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+
+                    // Map the claims found in idToken and/or userInfo
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+                    if(idToken.containsClaim("roles")){
+                        for (String role : idToken.getClaimAsStringList("roles")) {
+                            mappedAuthorities.add(new SimpleGrantedAuthority(role));
+                        }
+                    }
+
+                }
+            });
+
+            return mappedAuthorities;
+        };
     }
 }
