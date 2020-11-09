d9a343f0d3544c445c75417134ee72553a7ebb42
Christof <xstofgit@outlook.com>
2020-11-09 20:30:46 +0100

    add project for b2c testing

---
+++ demo-spring-mvc-b2c/src/main/java/com/example/demospringmvcb2c/IndexController.java
@@ -0,0 +1,44 @@
+package main.java.com.example.demospringmvcb2c;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(Model model,
+						@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+						@AuthenticationPrincipal OidcUser oauth2User,
+						HttpServletRequest request) {
+		
+		Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+		model.addAttribute("userName", oauth2User.getName());
+		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+		model.addAttribute("userAttributes", oauth2User.getAttributes());
+		model.addAttribute("authorities", oauth2User.getAuthorities());
+
+		// this works when roles are provided by AAD in id_token:
+		// if(request.isUserInRole("writerole")){
+		// 	logger.info("The user is in writer role");
+		// }
+
+		// if(request.isUserInRole("readrole")){
+		// 	logger.info("The user is in read role");
+		// }
+
+		return "index";
+	}
+}
---
+++ demo-spring-mvc-b2c/src/test/java/com/example/demospringmvcb2c/Demospringmvcb2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringmvcb2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospringmvcb2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
