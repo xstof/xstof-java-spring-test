edca81ffdf5fa9aea0ec2a68b85d8cabde1d2881
Christof <xstofgit@outlook.com>
2021-01-19 09:20:08 +0100

    enhance readme

--- aad3auth/src/main/java/fta/identity/sample/aad3auth/Aad3AuthSecurityConfig.java
+++ aad3auth/src/main/java/fta/identity/sample/aad3auth/Aad3AuthSecurityConfig.java
@@ -22,5 +22,6 @@ public class Aad3AuthSecurityConfig  extends AADWebSecurityConfigurerAdapter {
         ;
 
         super.configure(http); // => this causes redirect to AAD to happen for pages other than root: '/'
+        // for default implementation see: https://github.com/Azure/azure-sdk-for-java/blob/c92f8071a051b9b6212529ac86955c5e972b9f16/sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapp/AADWebSecurityConfigurerAdapter.java#L42
     }
 }
